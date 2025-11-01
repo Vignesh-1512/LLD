2734b7b69b75991789de47c89d17ea887a1925f3
Vignesh-1512 <155795863+Vignesh-1512@users.noreply.github.com>
2025-11-01 12:52:26 +0530

    Update Singleton.java

--- src/SingletonPattern/Singleton.java
+++ src/SingletonPattern/Singleton.java
@@ -1,4 +1,19 @@
 package SingletonPattern;
 
+//Eager initialization (simplest)
+
+//Creates the instance at class load time.
+
 public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() { /* private to prevent external instantiation */ }
+
+    public static EagerSingleton getInstance() {
+        if(instance==null{
+            instance=new Singleton();  
+        }
+        return instance;
+    }
 }
+

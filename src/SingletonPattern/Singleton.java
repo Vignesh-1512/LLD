5b20b64cc562af259ac5bf272faa8737caa244af
Vignesh-1512 <155795863+Vignesh-1512@users.noreply.github.com>
2025-11-01 13:02:20 +0530

    Update Singleton.java

--- src/SingletonPattern/Singleton.java
+++ src/SingletonPattern/Singleton.java
@@ -17,3 +17,12 @@ public class Singleton {
     }
 }
 
+public class Main {
+    public static void main(String[] args) {
+        Singleton s=Singleton.getInstance();
+        Singleton s1=Singleton.getInstance();
+        System.out.println(s);
+        System.out.println(s1);
+        System.out.println(s==s1);
+    }
+}
